31db8e322a4727b30f2df1204e3c46ff05f56617
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-22 23:58:36 +0000

    Initial commit

---
+++ src/main/java/com/jasonstorey/hipchat/ai/Main.java
@@ -0,0 +1,13 @@
+package com.jasonstorey.hipchat.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
