06fdd4ce350aa5493966b5abb2450e43f2b7b1a6
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-23 02:22:15 +0000

    Parse messages from JSON file.

--- src/main/java/com/jasonstorey/hipchat/ai/Main.java
+++ src/main/java/com/jasonstorey/hipchat/ai/Main.java
@@ -1,13 +1,34 @@
 package com.jasonstorey.hipchat.ai;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static List<Message> parseMessages(File jsonFile) {
+        List<Message> messages = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            messages = objectMapper.readValue(jsonFile, new TypeReference<List<Message>>(){});
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return messages;
+    };
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/jasonstorey/hipchat/ai/Message.java
@@ -0,0 +1,31 @@
+package com.jasonstorey.hipchat.ai;
+
+public class Message {
+    private String name;
+    private String content;
+    private String time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
