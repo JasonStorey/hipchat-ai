f17920671319624a69fa45603494b69599b5bb85
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-23 00:28:19 +0000

    Add rudimentary script for scraping hipchat archives.

---
+++ scripts/scrape-hipchat.js
@@ -0,0 +1,22 @@
+var messages = [];
+
+var rows = document.querySelectorAll('.hc-chat-row.hc-msg-nocolor');
+
+[].forEach.call(rows, function(row) {
+    var message,
+        name = row.querySelector('.hc-chat-from'),
+        content = row.querySelector('.hc-chat-msg div'),
+        time = row.querySelector('a');
+
+    if(name && content && time) {
+        message = {
+            name: name.innerHTML,
+            content: content.innerHTML,
+            time: time.innerHTML
+        };
+    }
+
+    messages.push(message);
+});
+
+console.log(JSON.stringify(messages));
