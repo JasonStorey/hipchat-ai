9b8a71833c9db64d801fe709d110473c282ed217
Jason Storey <jason.andrew.storey@gmail.com>
2015-01-24 01:13:47 +0000

    Optimise imports

--- src/main/java/com/jasonstorey/hipchat/ai/Tokeniser.java
+++ src/main/java/com/jasonstorey/hipchat/ai/Tokeniser.java
@@ -1,7 +1,5 @@
 package com.jasonstorey.hipchat.ai;
 
-import java.util.List;
-
 public class Tokeniser {
     public static String[] extractTokens(String text) {
         String[] tokens = text.trim().split("\\s+");
